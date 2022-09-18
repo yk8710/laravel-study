174fd4fe57e620a45228358c60e35dd8a5efbf16
yk8710 <y.kaneko198710@gmail.com>
2022-09-18 17:18:26 +0900

    フォームとHTTPリクエストメソッド

--- app/Http/Controllers/RequestSampleController.php
+++ app/Http/Controllers/RequestSampleController.php
@@ -27,4 +27,22 @@ class RequestSampleController extends Controller
         $url = route('profile', ['id' => 1, 'photos' => 'yes']);
         return 'プロフィールページのURLは'. $url;
     }
+
+    public function form()
+    {
+        return view('form');
+    }
+
+    public function loginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        if($request->get('email') == 'user@example.com' && $request->get('password') === '12345678'){
+            return 'ログイン成功';
+        }
+        return 'ログイン失敗';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,7 @@ Route::get('/query-strings', [RequestSampleController::class, 'queryStrings']);
 Route::get('/users/{id}', [RequestSampleController::class, 'profile'])->name('profile');
 Route::get('/products/{category}/{year}', [RequestSampleController::class, 'productsArchive']);
 Route::get('/route-link', [RequestSampleController::class, 'routeLink']);
+
+// ログイン
+Route::get('/login', [RequestSampleController::class, 'loginForm']);
+Route::post('/login', [RequestSampleController::class, 'login'])->name('login');
